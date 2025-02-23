61f1b5337df5dd25383acec0c18c4d08172a98c9
josenerydev <josenerydev@gmail.com>
2025-02-23 16:50:47 -0300

    atualiza lógica de upload de arquivos para gerar nomes únicos e altera imagem do serviço web no stack

--- playbooks/app/html/upload.php
+++ playbooks/app/html/upload.php
@@ -1,11 +1,16 @@
-<?php include 'includes/config.php';
+<?php 
+include 'includes/config.php';
 
 $response = ['status' => 'error', 'message' => 'Erro ao fazer upload.'];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['file']) && isset($_POST['dir'])) {
     $dirKey = $_POST['dir'];
     if (isset($directories[$dirKey])) {
-        $targetFile = $directories[$dirKey] . '/' . basename($_FILES['file']['name']);
+        $uploadDir = $directories[$dirKey];
+        $originalName = basename($_FILES['file']['name']);
+        $uniqueName = uniqid() . '-' . $originalName;
+        $targetFile = $uploadDir . '/' . $uniqueName;
+        
         if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFile)) {
             $response = ['status' => 'success', 'message' => "Upload feito com sucesso."];
         }
